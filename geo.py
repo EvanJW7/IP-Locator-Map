b12c0806224ca4d73695cf8f68c829b146cde195
Evan Wright <84414002+EvanJW7@users.noreply.github.com>
2023-08-16 23:10:36 -0500

    Update geo.py

--- geo.py
+++ geo.py
@@ -3,24 +3,18 @@ import subprocess
 import re
 import folium
 
-website = 'binance.com'
-print(f"Destination: {website}\n")
-print("Tracing route to desired destination...")
-print("This may take a few minutes")
+target_website = 'binance.com'
 
-
-def run_tracepath(website):
+#Get raw tracepath output
+def run_traceroute(website):
     try:
-        tracepath_output = subprocess.check_output(["traceroute", website], universal_newlines=True)
-        return tracepath_output
+        traceroute_output = subprocess.check_output(["traceroute", website], universal_newlines=True)
+        return traceroute_output
     except subprocess.CalledProcessError as e:
         print("Error running tracepath:", e)
         return None
 
-
-raw_data = run_tracepath(website)
-
-
+#Extract IP addresses from raw traceroute output
 def extract_ip_addresses(tracepath_output):
     ip_addresses = []
     if tracepath_output:
@@ -32,36 +26,52 @@ def extract_ip_addresses(tracepath_output):
     return ip_addresses
 
 
-ip_addresses = extract_ip_addresses(raw_data)
-ip_addresses.append(ip_addresses.pop(0))
-latitudes = []
-longitudes = []
+def main():
+    print(f"\nDestination: {target_website}\n")
+    print("Tracing route to desired destination...")
+    print("This may take a few minutes")
 
-print("\nFiltered data: ")
-for ip in ip_addresses:
-    try:
-        url = f"http://ip-api.com/json/{ip}"
-        response = requests.get(url)
-        data = response.json()
-        print(f"{ip:>25}     {data['city']}, {data['regionName']}")
-        latitudes.append(data['lat'])
-        longitudes.append(data['lon'])
-    except:
-        print(f"{ip:>25}     Private IP, unable to locate")
+    raw_data = run_traceroute(target_website)
+    ip_addresses = extract_ip_addresses(raw_data)
+    ip_addresses.append(ip_addresses.pop(0))
+    latitudes = []
+    longitudes = []
+    cities = []
 
-m = folium.Map(location=[latitudes[0], longitudes[0]], zoom_start=2)
+    #Print IPs and their location and map them
+    print("\nFiltered data: ")
+    for ip in ip_addresses:
+        try:
+            url = f"http://ip-api.com/json/{ip}"
+            response = requests.get(url)
+            data = response.json()
+            print(f"{ip:>25}     {data['city']}, {data['regionName']}, {data['country']}")
+            latitudes.append(data['lat'])
+            longitudes.append(data['lon'])
+            if data['city'] not in cities:
+                cities.append(data['city'])
+        except:
+            print(f"{ip:>25}     Private IP, unable to locate")
 
-# Add markers for each set of coordinates
-for lat, lon in zip(latitudes, longitudes):
-    folium.Marker([lat, lon]).add_to(m)
+    print(f"\nCongrats! Your internet connection made it all the way to {data['city']}")
+    print(f"Total IPs visited: {len(ip_addresses)}")
+    print(f"Total cities visited: {len(cities)}")
 
-lines = folium.PolyLine(
-    locations=list(zip(latitudes, longitudes)),
-    color='blue',
-    weight=2,
-    opacity=1
-).add_to(m)
+    m = folium.Map(location=[latitudes[0], longitudes[0]], zoom_start=2)
 
-# Save the map to an HTML file
-m.save('coordinates_map.html')
+    for lat, lon in zip(latitudes, longitudes):
+        folium.Marker([lat, lon]).add_to(m)
+
+    folium.PolyLine(
+        locations=list(zip(latitudes, longitudes)),
+        color='blue',
+        weight=2,
+        opacity=1
+    ).add_to(m)
+
+    m.save('ip_coordinates_map.html')
+
+
+if __name__ == '__main__':
+    main()
 
